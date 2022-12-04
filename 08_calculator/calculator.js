801cf9ebb45898a7cc22571f0fcfbd205f488f68
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2022-12-04 12:41:33 +0000

    Added 08Calc factorial solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -8,6 +8,7 @@ const subtract = function(num1, num2) {
 
 const sum = function(sumArray) {
   initalValue = 0;
+
   finalSum = sumArray.reduce((totalValue, currentValue) => {
     return totalValue += currentValue;
   }, initalValue);
@@ -25,14 +26,40 @@ const multiply = function(mutiplicationArray) {
 
 const power = function(numValue, powerIn) {
   let powerValue = 0;
+
 	for(let i = 0; i < powerIn){
     powerValue = numValue * numValue;
-  }
+  };
+
   return powerValue;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  //Recursive Solution - Taken from https://www.freecodecamp.org/news/how-to-factorialize-a-number-in-javascript-9263c89a4b38/
+	if(num < 0){
+    return -1;
+  } else if (num === 0 || num === 1){
+    return 1;
+  } else {
+    return (num * frctorial(num -1));
+  };
+  //Rcursive Soltuion - End
+
+
+  //forLoop Solution - Start
+
+  // if(num === 0 || num ===1){
+    // return 1;
+  // };
+// 
+  // for(i = num-1; i >= num; i--){
+    // num = num * i;
+  // };
+// 
+  // return num;
+  //forLoop Solution - End
+
+
 };
 
 // Do not edit below this line
