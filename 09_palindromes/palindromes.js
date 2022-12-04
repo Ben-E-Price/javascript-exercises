de95c085de579c394a384afffc38c53f50feade1
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2022-12-04 18:03:46 +0000

    09 String seperation Removal of illegal chars

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,67 @@
-const palindromes = function () {
+const palindromes = function (inString) {
+    //Santize String func
+        //Remove all number chars
+        //Remove all punctuatuon chars
+        //Serperate words
+            //loop until space char
+                //use loop count value to
+                    //add chars to new string at looppos
+                    //Remove chars from original string
+                    //Add seperated string to array of strings
+        //retrun arrayOfStrings
 
+    function sanitizeString(inString) {
+        inString = inString.toLowerCase(inString);
+        const arrayOfStrings = inString.split(" ");
+
+        //Remove punctuation + numbers
+        function removeIllegalChars(inStrings) {
+            const legalChars = RegExp(["[a-z]"]);
+            let cleanStrings = [];
+
+            //Loops over each array position containing a stiring
+            for(let i = 0; i < inStrings.length; i++){
+                let currentString = inStrings[i];
+                const currentStringLength = currentString.length -1;
+                
+                //Checks each char within the strgin
+                for(let i = currentStringLength; i >= 0; i--){
+                    const currentChar = currentString.at(i)
+
+                    //Removes illegal chars from current string
+                    if(!legalChars.test(currentChar)){
+                        currentString = currentString.replace(currentChar, "");
+                    };
+                };
+
+                //Ignore empty strings
+                if(currentString.length <= 0){
+                    continue
+                } else {
+                    cleanStrings[cleanStrings.length] = currentString;
+                };
+            };
+
+            return cleanStrings;
+        };
+        
+        console.log(removeIllegalChars(arrayOfStrings))
+
+        return arrayOfStrings;
+    };
+
+    //reverse input string
+    sanitizeString(inString);
+    let reverseString = "";
+
+    for(let i = inString.length -1; i >= 0; i--){
+        reverseString.concat(inString.charAt(i));
+    }
+
+//Comapre newString with input string
 };
 
+palindromes("rIn432 sdfs 345 ospd%f sdfsdf fred 45357 qsvcr45yb ggn&");
+
 // Do not edit below this line
-module.exports = palindromes;
+// module.exports = palindromes;
