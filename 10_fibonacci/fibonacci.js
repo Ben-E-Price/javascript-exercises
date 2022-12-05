c25cf7e38d14dbb115c49b82b28720f62f344ff9
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2022-12-05 18:18:08 +0000

    10Fibonacci Implemented sequence creation

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,37 @@
-const fibonacci = function() {
+const fibonacci = function(fibPosNum) {
+    //convert strings to nums - Account for negative values <0 
+    function cleanInput(inputNum) {
+        inputNum = Number(inputNum);
+        return inputNum <= 0? 0 : inputNum; 
+    };
 
+    function calcSeq(posNum) {
+        const fibValues = [];
+        let currentFibValue = 1; //Defines start value
+    
+        //Loop based on input
+        for(let i = 0; i < fibPosNum; i++){
+    
+            //Creates position 1 - 2 values
+            if(fibValues.length < 2){
+                fibValues[i] = currentFibValue;
+            } else {
+                currentFibValue = fibValues[i -2] + fibValues[i -1]
+                fibValues[i] = currentFibValue;
+            };
+        };
+
+        return fibValues
+    };
+
+    const sanInput = cleanInput(fibPosNum);
+    const fibValues = calcSeq(cleanInput);
+
+
+    return fibValues[sanInput -1];
 };
 
+console.log(fibonacci(1));
+
 // Do not edit below this line
-module.exports = fibonacci;
+// module.exports = fibonacci;
