deae30417e9fec10dab903becf4ac6769c6a9024
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2022-12-05 19:45:39 +0000

    Created findTheOldest solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    const currentYear = new Date().getFullYear();
+    
+    const oldestPerson = peopleArray.reduce((currentOldest, currentPerson) => {
+        //Check for death keypair
+        if(yearOfDeath in currentPerson){
+            currentPerson.age = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+        };
 
+        //Checks still alive
+        if(!yearOfDeath in currentPerson){
+            currentPerson.age = currentYear - currentPerson.yearOfBirth;
+        };
+ 
+        return currentPerson.age > currentOldest.age ? currentPerson : currentOldest;
+    });
+
+    return oldestPerson;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
